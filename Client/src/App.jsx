c5b5bff9d95be6d890fba45fed378e7e7272ed40
GuillaumeRcd <120723419+GuillaumeRcd@users.noreply.github.com>
2023-10-12 12:14:59 +0200

    Api fonctionne, reste à determiner le rayon + styling sur le front + trouver un moyen de lire plus rapidement le csv

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,41 +1,9 @@
-import { useState, useEffect } from "react";
-// import { fetchNetworkCoverage } from "./APIS/api.backend";
-// import Select from "react-select";
+import { useState } from "react";
 import AsyncSelect from "react-select/async";
-// import { searchAddress } from './APIS/api.data.gouv';
 
 function App() {
-  // const [address, setAddress] = useState("");
   const [coverage, setCoverage] = useState({});
-  const [proxyResponse, setProxyResponse] = useState(null);
-  // const [suggestions, setSuggestions] = useState([]);
-
-  // const handleAddressChange = (selectedOption) => {
-  //   // selectedOption contient la nouvelle valeur sélectionnée
-  //   const newAddress = selectedOption ? selectedOption.value : ''; // Obtenez la valeur sélectionnée ou une chaîne vide si rien n'est sélectionné
-  //   setAddress(newAddress);
-  // };
-
-  // useEffect(() => {
-  //   if (address.length > 5) {
-  //     fetch(`http://localhost:8000/proxy/?address=${address}`)
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         const features = data.features;
-  //         const labels = features.map((feature) => feature.properties.label);
-  //         setSuggestions(labels);
-  //         console.log(suggestions);
-  //       })
-  //       .catch((error) => console.error("Erreur lors de la requête:", error));
-  //   } else {
-  //     setSuggestions([]);
-  //   }
-  // }, [address]);
-
-  // const formattedSuggestions = suggestions.map((suggestion) => ({
-  //   label: suggestion,
-  //   value: suggestion,
-  // }));
+  const [selectedAddress, setSelectedAddress] = useState(null);
 
   const loadOptions = (inputValue, callback) => {
     if (inputValue.length > 5) {
@@ -43,9 +11,14 @@ function App() {
         .then((response) => response.json())
         .then((data) => {
           const features = data.features;
+          console.log(features);
           const labels = features.map((feature) => ({
             label: feature.properties.label,
             value: feature.properties.label,
+            x: feature.properties.x,
+            y: feature.properties.y,
+            long: feature.geometry.coordinates[0],
+            lat: feature.geometry.coordinates[1],
           }));
           callback(labels);
         })
@@ -55,57 +28,45 @@ function App() {
     }
   };
 
-  // const fetchProxyResponse = async () => {
-  //   try {
-  //     console.log(address);
-  //     const response = await fetch(
-  //       `http://localhost:8000/proxy/?address=${address}`
-  //     );
-  //     const data = await response.json();
-  //     setProxyResponse(data.features);
-  //     console.log(data.features);
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
+  const handleSearchNetwork = async () => {
+    if (selectedAddress) {
+      console.log(selectedAddress);
 
-  // const fetchCoverage = async () => {
-  //   try {
-  //     const data = await fetchNetworkCoverage(address);
-  //     setCoverage(data);
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
+      try {
+        const response = await fetch("http://localhost:8000/coverage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(selectedAddress),
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        console.log("Réponse du backend :", data);
+      } catch (error) {
+        console.error("Erreur lors de la requête vers le backend:", error);
+      }
+    }
+  };
 
   return (
     <div>
       <h1>Network Coverage Checker</h1>
       <div>
-        {/* <input
-          type="text"
-          placeholder="Enter address..."
-          value={address}
-          onChange={handleAddressChange}
-        /> */}
-        {/* 
-        <Select
-          type="text"
-          placeholder="Enter address..."
-          value={address}
-          onChange={handleAddressChange}
-          options={formattedSuggestions}
-          isSearchable={true}
-          // onChange={(selectedOption) => setAddress(selectedOption.value)}
-        /> */}
         <AsyncSelect
           cacheOptions
           defaultOptions
           loadOptions={loadOptions}
           isSearchable={true}
+          onChange={(selectedOption) => setSelectedAddress(selectedOption)}
         />
-        {/* <button onClick={fetchCoverage}>Check Coverage</button>
-        <button onClick={fetchProxyResponse}>Fetch Proxy Response</button> */}
+      </div>
+      <div>
+        <button onClick={handleSearchNetwork}>Chercher réseau</button>
       </div>
       <div>
         <h2>Network Coverage:</h2>
--- Server/main.py
+++ Server/main.py
@@ -3,56 +3,113 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import httpx
+import pyproj
+import pandas as pd
+from shapely.geometry import Point
 
 app = FastAPI()
+
 
 class AddressRequest(BaseModel):
     text: str
 
+
+class AddressData(BaseModel):
+    label: str
+    value: str
+    x: float
+    y: float
+    long: float
+    lat: float
+
+
 stored_address = ""
-    
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"], 
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+# @app.post("/network/")
+# async def test(request: AddressRequest):
+#     global stored_address
+#     stored_address = request.text
+#     print(request.text)
+#     return {"message": f"Adresse reçue : {stored_address}"}
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+# @app.get("/network/", response_model=dict)
+# async def get_network_data():
+#     global stored_address
+#     return {"message": "Données réseau obtenues avec succès", "address": stored_address}
 
-@app.post("/network/")
-async def test(request: AddressRequest):
-    global stored_address
-    stored_address = request.text
-    print(request.text)
-    return {'message': f'Adresse reçue : {stored_address}'}
-
-
-@app.get("/network/", response_model=dict)
-async def get_network_data():
-    global stored_address
-    return {"message": "Données réseau obtenues avec succès", "address": stored_address}
 
 @app.get("/proxy/")
 async def proxy_address(address: str):
-    print(address)
+    # print(address)
     if not address:
         raise HTTPException(status_code=400, detail="Address is required")
 
     try:
         async with httpx.AsyncClient() as client:
-            response = await client.get(f"https://api-adresse.data.gouv.fr/search/?q={address}")
-            response.raise_for_status() 
+            response = await client.get(
+                f"https://api-adresse.data.gouv.fr/search/?q={address}"
+            )
+            response.raise_for_status()
 
         return response.json()
     except httpx.HTTPError as e:
-        raise HTTPException(status_code=500, detail="An error occurred while fetching data") from e
+        raise HTTPException(
+            status_code=500, detail="An error occurred while fetching data"
+        ) from e
+
+
+df = pd.read_csv(
+    # "data/2018_01_Sites_mobiles_2G_3G_4G_France_metropolitaine_L93.csv", delimiter=";"
+    "data/test.csv", delimiter=";"
+)
+
+
+@app.post("/coverage")
+async def get_network_coverage(address_data: AddressData):
+    print(address_data)
+    user_point = Point(address_data.long, address_data.lat)
+    rayon = 10
+    result = {}
+    
+    operator_mapping = {
+        20801: "Orange",
+        20810: "SFR",
+        20815: "Free",
+        20820: "Bouygues"
+    }
+    
+    for index, row in df.iterrows():
+        x = row['x']
+        y = row['y']
+        operator_code = row['Operateur']
+        operator_name = operator_mapping.get(operator_code)
+
+        lon, lat = lamber93_to_gps(x, y)
+        cercle = Point(lon, lat).buffer(rayon)
+        
+        if cercle.intersects(user_point) :
+            result[operator_name] = {"2G": False, "3G": False, "4G": False}
+            result[operator_name]["2G"] = bool(row['2G'])
+            result[operator_name]["3G"] = bool(row['3G'])
+            result[operator_name]["4G"] = bool(row['4G'])
+
+    return result
+
+
+def lamber93_to_gps(x, y):
+    lambert = pyproj.Proj(
+        "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+    )
+    wgs84 = pyproj.Proj("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    long, lat = pyproj.transform(lambert, wgs84, x, y)
+    return long, lat
